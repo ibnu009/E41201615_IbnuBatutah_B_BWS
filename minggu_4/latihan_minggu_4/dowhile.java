ae04318c2ee58ca6462da01a0ff03d1014619ef9
ibnu009 <ibnubatutah001@gmail.com>
2021-03-29 22:19:25 +0700

    Upload Latihan Minggu 4

---
+++ minggu_4/latihan_minggu_4/dowhile.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu4;
+
+/**
+ *
+ * @author HP
+ */
+public class dowhile {
+    public static void main (String[] args) {
+        // membuat variabel
+        int i =  0;
+        
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while ( i <= 10);
+        
+        }
+    }
